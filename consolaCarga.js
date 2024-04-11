b6e653940195d79bc2fd9f1570e5a7a856dfa82c
chacin04 <angelgabriel4.ac@gmail.com>
2024-04-11 10:24:05 -0400

    feat , convertirdo en funcion

--- consolaCarga.js
+++ consolaCarga.js
@@ -1,1 +1,5 @@
-process.stdout.write(`\r [${(" ▮ ").repeat(Math.trunc((((index+1)*100)/pricelist.length)/10))}${(' ▯ ').repeat(10-(Math.trunc((((index+1)*100)/pricelist.length)/10)))} ] ${pricelist.length} // ${index+1} // ${((((index+1)*100)/pricelist.length)).toFixed(2)} %`)
+function loaderConsola (index,data){
+    process.stdout.write(`\r [${(" ▮ ").repeat(Math.trunc((((index+1)*100)/data.length)/10))}${(' ▯ ').repeat(10-(Math.trunc((((index+1)*100)/data.length)/10)))} ] ${data.length} // ${index+1} // ${((((index+1)*100)/data.length)).toFixed(2)} %`)
+
+}
+export default loaderConsola
